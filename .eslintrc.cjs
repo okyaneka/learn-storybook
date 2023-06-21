841f64f9ba9f928505679e352b50aa47e5203812
OKY <okyaneka@gmail.com>
2023-06-21 16:31:29 +0700

    init storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,5 @@
 /* eslint-env node */
 require('@rushstack/eslint-patch/modern-module-resolution')
-
 module.exports = {
   root: true,
   node: true,
@@ -8,7 +7,8 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
+    'plugin:storybook/recommended'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
